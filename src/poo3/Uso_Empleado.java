9c583bf4bb40ed47ade98a72e6128d9dd1a0b291
jonberge1337 <jonbergerandi@gmail.com>
2019-03-01 13:47:16 +0100

    nuevo paquete empleado

---
+++ src/poo3/Uso_Empleado.java
@@ -0,0 +1,28 @@
+package poo3;
+
+import java.util.*;
+
+public class Uso_Empleado {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+class Empleado {
+	
+	public Empleado(String nom, double sue, int agno, int mes, int dia) {
+		
+		
+		
+	}
+	
+	private String nombre;
+	
+	private double sueldo;
+	
+	private Date altaContrato;
+
+}
