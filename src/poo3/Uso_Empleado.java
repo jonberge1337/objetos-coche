08860367552ce3c01a9010d989b53adcd5d5d6b9
jonberge1337 <jonbergerandi@gmail.com>
2019-03-01 14:31:17 +0100

    modificaciones DAte

--- src/poo3/Uso_Empleado.java
+++ src/poo3/Uso_Empleado.java
@@ -15,8 +15,32 @@ class Empleado {
 	
 	public Empleado(String nom, double sue, int agno, int mes, int dia) {
 		
+		nombre = nom;
 		
+		sueldo = sue;
+
+		GregorianCalendar calendario = new GregorianCalendar(agno, mes-1, dia);
 		
+		altaContrato = calendario.getTime();
+		
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public double getSueldo() {
+		return this.sueldo;
+	}
+	
+	public Date getAltaContrato() {
+		return this.altaContrato;
+	}
+	
+	public void setSubeSueldo(double porcentaje) {
+		double aumento = this.sueldo * porcentaje / 100;
+		
+		this.sueldo += aumento;
 	}
 	
 	private String nombre;
