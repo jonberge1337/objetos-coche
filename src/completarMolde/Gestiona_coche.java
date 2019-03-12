7fb78a2b2ce928c0766dd78a1c8ac7d0883ad266
jonberge1337 <jonbergerandi@gmail.com>
2019-03-12 20:37:18 +0100

    el coche esta a punto

--- src/completarMolde/Gestiona_coche.java
+++ src/completarMolde/Gestiona_coche.java
@@ -3,7 +3,7 @@ package completarMolde;
 import java.util.Scanner;
 
 public class Gestiona_coche {
-	
+
 	public static int elegirMenu() {
 		int menu;
 		@SuppressWarnings("resource")
@@ -11,55 +11,86 @@ public class Gestiona_coche {
 		System.out.println("Introduce un numero de las siguientes opciones: ");
 		System.out.println("1. Acelerar \n2. Frenar \n3. Ver velocidad \n4. salir");
 		do {
-			menu = sc.nextInt();
+			try {
+				menu = sc.nextInt();
+			} catch (Exception e) {
+				menu = 0;
+				sc.nextLine();
+			}
+
 			if(menu > 4 || menu < 1) {
 				System.out.println("Introduce una opcion correcta");
 			}
 		} while (menu > 4 || menu < 1);
-		
-		
+
+
 		return menu;
 	}
-	
+
 	public static void acelera(Coche coche) {
 		int velocidad;
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-		
-			System.out.println("Introduce en cuanto quieres aumentar la velocidad: ");
-			
-			velocidad = sc.nextInt();
-			if(velocidad + coche.getVelocidad() > 120) {
-				System.out.println("Prohibido aumentar 120 km/h");
-			}else {
-				coche.acelera(velocidad);
+
+		System.out.println("Introduce en cuanto quieres aumentar la velocidad: ");
+		do {
+			try {
+				velocidad = sc.nextInt();
+			} catch (Exception e) {
+				velocidad = 0;
+				sc.nextLine();
 			}
-				
+			if(velocidad < 1) {
+				System.out.println("Tienes que meter un numero mayor que 0");
+			}
+		} while (velocidad < 1);
+		
+		if(velocidad + coche.getVelocidad() > 120) {
+			coche.acelera(120 - coche.getVelocidad());;
+		} else {
+			coche.acelera(velocidad);
+		}
+
 	}
-	
+
 	public static void frenar(Coche coche) {
 		int velocidad;
-		
+
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce en cuanto quieres frenar la velocidad");
-		
-		velocidad = sc.nextInt();
+		do {
+			try {
+				velocidad = sc.nextInt();				
+			} catch (Exception e) {
+				velocidad = 0;
+				sc.nextLine();
+			}
+			if(velocidad < 1) {
+				System.out.println("Tienes que meter un numero mayor que 0");
+			}
+		} while (velocidad < 1);
+
 		if(coche.getVelocidad() - velocidad < 0) {
-			System.out.println("No puedes frenar tanto bajarias de 0 km/h");
-		}else {
+			coche.frena(coche.getVelocidad());
+		} else {
 			coche.frena(velocidad);
 		}
 	}
-	
+
 	public static void verVelocidad(int velocidad) {
-		System.out.println("Vas a " + velocidad + "km/h");
+		if(velocidad == 0) {
+			System.out.println("Estas parado");
+		} else {
+			System.out.println("Vas a " + velocidad + "km/h");
+		}
+
 	}
-	
+
 	public static void main(String[] args) {
 		//programa que cree un coche , a continuacion mostrara un menu al usuario con las opciones 
 		//acelerar frenar, ver velocidad, y salir.
-		
+
 		int menu;
 		Coche bmw = new Coche();
 		do {
@@ -71,7 +102,7 @@ public class Gestiona_coche {
 			default: System.out.println("Programa finalizado");	break;
 			}
 		} while (menu != 4);
-		
+
 	}
 
 }
